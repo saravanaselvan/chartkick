66ff64785783586a2d99d5b741c5b897610978ad
Saravanaselvan SP <spsaravanaselvan@gmail.com>
2015-10-30 15:41:05 +0530

    updated for older ruby

--- lib/chartkick.rb
+++ lib/chartkick.rb
@@ -17,7 +17,7 @@ module Enumerable
   def chart_json
     if is_a?(Hash) && (key = keys.first) && key.is_a?(Array) && key.size == 2
       group_by { |k, _v| k[0] }.map do |name, data|
-        {name: name, data: data.map { |k, v| [k[1], v] }}
+        {:name => name, :data => data.map { |k, v| [k[1], v] }}
       end
     else
       self
